e282c73c87af993201fc299883fc21b9b299987c
Ekansh Gpta <ekanshg2007@gmail.com>
2026-09-19 18:07:39 +0530

    Time: 4 ms (25.83%), Space: 8.8 MB (67.64%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i=1,count=0;
+        while(n>=0){
+            n-=i;
+            i++;
+            if(n>=0)
+            count++;
+        }
+        return count;
+    }
+};
